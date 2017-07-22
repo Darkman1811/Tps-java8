463aafd29f14eb44c8337061be1233f0c5afaa22
Abdoulaye Diaw <abdoulayediaw6@hotmail.com>
2017-07-22 17:03:22 +0000

    Collections

--- src/tpsjava8/TpsJava8.java
+++ src/tpsjava8/TpsJava8.java
@@ -15,7 +15,7 @@ public class TpsJava8 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello word laye diaw");
+        System.out.println("Using Collections");
     }
     
 }
