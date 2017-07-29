45c72e79896eed7c4eae74d99912a4ffb8b69f92
Abdoulaye Diaw <abdoulayediaw6@hotmail.com>
2017-07-29 15:11:59 +0000

    Using flatMaps

--- src/tpsjava8/TpsJava8.java
+++ src/tpsjava8/TpsJava8.java
@@ -24,14 +24,26 @@ public class TpsJava8 {
      */
     public static void main(String[] args) {
         System.out.println("Using Streams");
-        Optional<String> str=Optional.ofNullable("Hello");
-        str.ifPresent(System.out::println);
+        //Stream(int)->Stream(Stream(int,qrt))
+        Stream.of(1,2,3).map(n->Stream.of(n,n*n)).forEach(System.out::println);
         
-        Optional<String> str2=Optional.ofNullable(null);
-        str2.ifPresent(System.out::println);
-        IntStream.of(1,4,56,6,12,57).filter(n->n%2==1).max().ifPresent(System.out::println);
-        IntStream.of(10,4,56,6,12,58).filter(n->n%2==1).max().ifPresent(System.out::println);
+        //Conciderer la valeur renvoyé comme un stream et lui appliquer les bonnes methodes
+         Stream.of(1,2,3).map(n->Stream.of(n,n*n)).forEach(e->e.forEach(System.out::println));
+         
+         //Using flatMap
+         Stream.of(1,2,3).flatMap(n->Stream.of(n,n*n)).forEach(System.out::println);
         
+         String[] strs1={"Dakar","Diourbel","Thies"};
+         String[] strs2={"Goloulou","Dioubola","Tinon"};
+         String[] strs3={"Sissi","Sassilou","Simai"};
+        
+         List<String[]> pays=new ArrayList<>();
+         pays.add(strs1);
+         pays.add(strs2);
+         pays.add(strs3);
+         //if the inner element is not a stream you need to convert it to a stream
+         pays.stream().flatMap(n->Arrays.stream(n)).forEach(System.out::println); //if the inner element is not a stream
+         
     }
 
 }
