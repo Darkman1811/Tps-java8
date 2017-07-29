bf71b7e2ff390e4818a9406a20ee4b9e64415a3f
Abdoulaye Diaw <abdoulayediaw6@hotmail.com>
2017-07-29 11:41:18 +0000

    First Simple  stream

--- src/tpsjava8/TpsJava8.java
+++ src/tpsjava8/TpsJava8.java
@@ -5,6 +5,10 @@
  */
 package tpsjava8;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *
  * @author LaptopWin
@@ -15,7 +19,14 @@ public class TpsJava8 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Using Streams");
+        System.out.println("Using Streams");      
+        List <Integer> numbers=Arrays.asList(1,2,3,4,5);
+        int sum=numbers.stream()
+                       .filter(n->n%2==1)
+                       .map(n->n*n)
+                       //.reduce(0, Integer::sum);
+                       .reduce(0,(n1,n2)->n1+n2);
+        System.out.println(sum);
     }
     
 }
